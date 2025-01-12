f342e2ef6d558d7db186db6e5ab02215f1adbc95
unknown <varshasirsat849@gmail.com>
2025-01-12 15:26:45 +0530

    varsha search product test cases

--- src/main/java/com/bigbasket/pages/HomePage.java
+++ src/main/java/com/bigbasket/pages/HomePage.java
@@ -1,9 +1,12 @@
 package com.bigbasket.pages;
 
+import static org.testng.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,6 +22,13 @@ public class HomePage {
 	
 	@FindBy(xpath="//li[@class=\"jsx-1259984711\" and @role=\"none\"]")
 	List<WebElement> categoryList;
+	
+	@FindBy(xpath = "(//input[@placeholder=\"Search for Products...\"])[2]")
+	WebElement SearchTextBox;
+
+	@FindBy(xpath = "//li[@class=\"QuickSearch___StyledMenuItem-sc-rtz2vl-4 ibNDA-d\"]")
+	List<WebElement> results;
+
 	
 	public void verifyShopByCategoryMenuIsAvailable() {
 		Assert.assertTrue(shopByCategoryMenu.isDisplayed());
@@ -47,4 +57,54 @@ public class HomePage {
 		softlyAssert.assertEquals(actualCategoryNames, expectedCategoryNames);
 		softlyAssert.assertAll();		
 	}
+	
+	public void SearchTextBoxVisibleOrNotUsingPagefactory() {
+		System.out.println(SearchTextBox.isDisplayed());
+		Assert.assertTrue(SearchTextBox.isDisplayed());
+
+	}
+
+	public void sendKeysToElement() {
+		SearchTextBox.sendKeys("Apple");
+		Assert.assertEquals(SearchTextBox.getAttribute("value"), "Apple");
+
+	}
+
+	public HomePageUsingPageFactory() {
+		PageFactory.initElements(Keyword.driver, this);
+	}
+
+	public void enterTextPlaceholderTextIsEmptyUsingPgaeFactory() {
+		String value = SearchTextBox.getAttribute("value");
+		SearchTextBox.sendKeys("Apple");
+		String value1 = SearchTextBox.getAttribute("value");
+		Assert.assertFalse(value == value1);
+
+	}
+
+	public void RelevantResultIsDispalyedWhenEnterText() throws InterruptedException {
+		SearchTextBox.sendKeys("Tomato");
+		Thread.sleep(3000);
+		System.out.println(results.size());
+		for (WebElement result : results) {
+			String text = result.getText();
+			System.out.println(text);
+			assertTrue(text.contains("Tomato"));
+
+		}
+
+	}
+
+	public void verifyBehaviourOfSearchbarWhenLeftEmptyAndPressEnter() {
+		String urlBefore = Keyword.driver.getCurrentUrl();
+		System.out.println(urlBefore);
+		SearchTextBox.sendKeys("");
+		SearchTextBox.sendKeys(Keys.ENTER);
+		String urlAfter = Keyword.driver.getCurrentUrl();
+		System.out.println(urlAfter);
+		Assert.assertEquals(urlBefore, urlAfter);
+
+	}
+}
+
 }
---
+++ src/test/java/bigbasketTests/A.java
@@ -0,0 +1,9 @@
+package bigbasketTests;
+
+public class A {
+	public void m1(){
+		System.out.println("varsha");
+		
+	}
+
+}
